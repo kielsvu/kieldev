7b33cdb5144f50c55c602d98edfa4f6cb85260d4
kielsvu <jhoneragustin@gmail.com>
2026-09-20 18:19:37 +0800

    Update WelcomeScreen.tsx

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -194,7 +194,7 @@ export default function WelcomeScreen() {
             color: 'rgba(255,255,255,0.7)',
           }}
         >
-          Kiel
+          kiel.dev
         </motion.div>
       </motion.div>
     </div>
